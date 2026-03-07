5af5b2a347b51f3588bd4aca658019639f1f3706
Ujjal26 <156526573+Ujjal26@users.noreply.github.com>
2026-03-08 05:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ const table= document.querySelector(' #sampleTable');
 
 // btn.addEventListener('click',()=>{
     const newrow=document.createElement('tr');
-    newrow.classList.add('row');
     for(let i=1;i<=2;i++){
         const newdata= document.createElement('td');
-        newdata.classList.add('data');
         newdata.textContent= 'New Cell'+i;
         newrow.appendChild(newdata);
     }
