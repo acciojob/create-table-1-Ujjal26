8d82076fee8836b715e6f02a93d8d6f2ee03406f
Ujjal26 <156526573+Ujjal26@users.noreply.github.com>
2026-03-08 05:17:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
     //Write your code here
-  
+  const btn= document.querySelector('input');
+const table= document.querySelector(' #sampleTable');
+
+
+// btn.addEventListener('click',()=>{
+    const newrow=document.createElement('tr');
+    newrow.classList.add('row');
+    for(let i=1;i<=2;i++){
+        const newdata= document.createElement('td');
+        newdata.classList.add('data');
+        newdata.textContent= 'New Cell'+i;
+        newrow.appendChild(newdata);
+    }
+    table.prepend(newrow);
+// });
+
   
 }
